0d642c2857dd015f3458250f97f8db880b99d545
Heshan904 <heshanhasitha904@gmail.com>
2025-08-06 02:57:32 +0530

    upgrade

---
+++ onezerox_frontend/src/pages/Project.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import NaviBar from '../component/NaviBar'
+import Footer from '../component/Footer'
+
+function Project() {
+  return (
+    <>
+    <NaviBar/>
+    <div className="pmain">
+        <div class="card">
+            <div>
+                <div>
+                <img src="..."  alt="..."/>
+                </div>
+                <div >
+                <div >
+                    <h5 >Card title</h5>
+                    <p >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p ><small >Last updated 3 mins ago</small></p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default Project
